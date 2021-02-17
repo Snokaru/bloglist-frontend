a53d6b96410f5d1bc9592674dafa652a50ed78fd
Vlad <vlad.slivilescu@gmail.com>
2021-02-18 00:45:01 +0200

    Initialise frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const submitLoginForm = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await loginService.login({
+        username, password
+      });
+
+      setUser(user);
+      setPassword('');
+      setUsername('');
+    } catch (exception) {
+      
+    }
+  };
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
